1c600b2f17dd6c6e4eeeba44f6368bc77feb0d15
hoffmr2@wp.pl <hoffmr2@wp.pl>
2017-10-20 22:33:42 +0200

    done reading frames from webcam and established openCV

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,27 @@
+#include <opencv2/opencv.hpp>
 
-int main(int argc, char* argv[])
+using namespace cv;
+
+int main(int, char**)
 {
+
+  VideoCapture cap(0); // open the default camera
+  if (!cap.isOpened())  // check if we succeeded
+    return -1;
+
+  namedWindow("human height", 1);
+
+  // main loop:
+  for (;;)
+  {
+    Mat frame;
+    cap >> frame; // get a new frame from camera
+
+    imshow("human height", frame);
+    if (waitKey(30) >= 0) break;
+  }
+  // the camera will be deinitialized automatically in VideoCapture destructor
   return 0;
 }
+
+
